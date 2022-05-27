5e95888eacc2283adeb1bb8f79eabfccd840da0b
Inessa Modina <modina.inessa@gmail.com>
2022-05-27 10:01:22 -0700

    Update users to current stage

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import * as constants from "../constants/constants.js";
 
@@ -23,29 +22,3 @@ export function Users({users}) {
 		</div>
 	)
 }
-=======
-		import React from "react";
-		import * as constants from "../constants/constants.js";
-		
-		export function Users({users}) {
-			return (
-				<div className="app-section__users ">
-					{users.map((user) => (
-					<div key={user.id} className = {constants.USER_CARD_CLASS}>
-						<img src={user.avatar} className="app-section__member-img"
-								alt="User"/>
-						<p className="app-section__member-review">
-							{constants.USER_REVIEW}
-						</p>
-						<div className="app-section__member-name">
-							{user.firstName} {user.lastName}
-						</div>
-						<div className="app-section__member-position">
-							{user.position}
-						</div>
-					</div>
-					))}
-				</div>
-			)
-		}
->>>>>>> 745986d467c6bcca5bf7155bb0a99a7a77a5e364
